9d955c2249c18948e66cddf2dd1103816ebbd2d7
deciob <decio.battaglia@gmail.com>
2014-10-21 21:50:51 +0100

    changes to run config

--- js/run.js
+++ js/run.js
@@ -1,11 +1,6 @@
 (function () {
 
   curl.config({
-    baseUrl: '../',
-    packages: {
-      curl: { location: 'lib/curl/src/curl/' },
-      //app: { location: 'app', main: 'main' },   
-    },
     paths: {
       d3: 'bower_components/d3/d3.min.js',
       d3_tip: 'bower_components/d3-tip/index.js',
@@ -14,7 +9,7 @@
     }
   });
 
-  curl(['main']).then(start, fail);
+  curl(['js/main']).then(start, fail);
 
   function start(main) {
     // do something startup-ish
